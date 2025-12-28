3730de83d74f7db17c63b630ad47aa88ca45c320
Alizadeh, Rouhallah <R.Alizadeh@behsacorp.com>
2025-12-28 14:42:20 +0330

    fix test explorer

--- tests/unit/vitest.config.ts
+++ tests/unit/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',    
-    include: ['**/*.{test,spec}.ts'],
+    include: ['./**/*.{test,spec}.ts'],
     setupFiles: ['setup.ts'],
   },
 });
